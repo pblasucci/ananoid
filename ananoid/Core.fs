c7b6f7a5e38a38f651af267107ab18b1d00c050d
Paul Blasucci <pblasucci@gmail.com>
2023-05-29 21:38:11 +0200

    Merge `Core.fsi` into `Core.fs`

--- ananoid/Core.fs
+++ ananoid/Core.fs
@@ -3,6 +3,19 @@
   License, v. 2.0. If a copy of the MPL was not distributed with this
   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 *)
+
+/// <summary>
+/// Contains simple functions for generating 'nano identifier' strings
+/// (a simple alternative to things like 'Universal Unique Identifiers').
+/// </summary>
+///
+/// <namespacedoc>
+///   <summary>
+///   This library provides nano identifiers, an alternative to UUIDs (inspired
+///   by <a href="https://github.com/ai/nanoid">github.com/ai/nanoid</a>).
+///   </summary>
+/// </namespacedoc>
+///
 module pblasucci.Ananoid.Core
 
 #nowarn "9" (* unverifiable IL - see `Core.stackspan` function for details *)
@@ -27,7 +40,7 @@ let inline (|Length|) value =
   else
     Length(value.Trim() |> String.length |> uint32)
 
-let private generate (alphabet & Length length) size =
+let private generate (Length length as alphabet) size =
   let mask = (2 <<< 31 - LeadingZeroCount((length - 1u) ||| 1u)) - 1
   let step = int (ceil ((1.6 * float mask * float size) / float length))
 
@@ -52,35 +65,93 @@ let private generate (alphabet & Length length) size =
   nanoid.ToString()
 
 
+/// Defines the recommended set of characters and output length
+/// for optimally generating nano identifier strings.
 [<RequireQualifiedAccess>]
 module Defaults =
+  /// <summary>
+  /// An alphabet consisting of: URL-friendly numbers, English letters, and
+  /// symbols (ie: <c>A-Za-z0-9_-</c>).
+  /// This is the default alphabet if one is not explicitly specified.
+  /// </summary>
   [<Literal>]
   let Alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-"
 
+  /// Twenty-one (21) single-byte characters.
+  /// This is the default output lenght if one is not explicitly specified.
   [<Literal>]
   let Size = 21
 
 
+/// <summary>
+/// Generates a new identifier, <c>size</c> characters in length,
+/// derived from the letters of the given alphabet
+/// (note: a size of less than zero will result in an empty string).
+/// </summary>
+/// <exception cref="T:System.ArgumentOutOfRangeException">
+/// Thrown when <c>alphabet</c> contains &lt; 1 letter, or &gt; 255 letters.
+/// </exception>
 [<CompiledName("NewNanoId")>]
 let nanoIdOf (Length length as alphabet) size =
   if size < 1 then ""
   elif length < 1u || 255u < length then outOfRange (nameof alphabet)
   else generate alphabet size
 
+/// Generates a new identifier with the default alphabet and size.
 [<CompiledName("NewNanoId")>]
-let nanoId () = nanoIdOf Defaults.Alphabet Defaults.Size
+let nanoId () : string = nanoIdOf Defaults.Alphabet Defaults.Size
 
 
+/// <summary>
+/// Contains primitives for generating identifiers which are "tagged"
+/// with a discriminator (useful for managing lots of string which have
+/// different purposes, but where using a full CLR type is undesirable).
+/// </summary>
+/// <remarks>
+/// <b>This module is not intended for languages other than F#.</b>
+/// </remarks>
 module Tagged =
+  /// <summary>
+  /// An abbreviation for the CLI type System.String.
+  /// </summary>
+  /// <remarks>
+  /// <b>This alias is not usable from languages other than F#.</b>
+  /// </remarks>
   [<CompiledName("string@measurealias")>]
   [<MeasureAnnotatedAbbreviation>]
   type string<[<Measure>] 'Tag> = string
 
+  /// <summary>
+  /// A "tag", which can be used as a discriminator.
+  /// </summary>
+  /// <remarks>
+  /// <b>This tag is not usable from languages other than F#.</b>
+  /// </remarks>
   [<CompiledName("nanoid@measure")>]
   [<Measure>]
   type nanoid =
+    /// <summary>
+    /// Applies the <c>nanoid</c> "tag" to a string.
+    /// </summary>
+    /// <remarks>
+    /// <b>This function is not usable from languages other than F#.</b>
+    /// </remarks>
     static member tag value = (# "" (value : string) : string<nanoid> #)
 
+  /// <summary>
+  /// Generates a new tagged identifier, <c>size</c> characters in length,
+  /// derived from the letters of the given alphabet
+  /// (note: a size of less than zero will result in an empty string).
+  /// </summary>
+  /// <remarks>
+  /// <b>This function is not usable from languages other than F#.</b>
+  /// </remarks>
   let nanoIdOf' alphabet size = nanoid.tag (nanoIdOf alphabet size)
 
+  /// <summary>
+  /// Generates a new tagged identifier with the default alphabet and size.
+  /// </summary>
+  /// <remarks>
+  /// <b>This function is not usable from languages other than F#.</b>
+  /// </remarks>
   let nanoId' () = nanoIdOf' Defaults.Alphabet Defaults.Size
