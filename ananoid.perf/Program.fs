a70bc24601f59b82257e72767ac7523554d36cbb
Paul Blasucci <pblasucci@proton.me>
2026-02-18 08:45:57 +0000

    improved perf suite

--- ananoid.perf/Program.fs
+++ ananoid.perf/Program.fs
@@ -12,14 +12,14 @@ open BenchmarkDotNet.Running
 open NanoidDotNet
 open pblasucci.Ananoid
 open pblasucci.Ananoid.Core
-open LetterSets
+open pblasucci.Ananoid.Core.Alphabets
 
 
 [<MemoryDiagnoser>]
 [<SimpleJob(RunStrategy.Throughput)>]
 type AnanoidVsNanoidNetVsGuid() =
   [<Benchmark(Baseline = true)>]
-  member _.Guid() = Guid.NewGuid()
+  member _.Guid() = Guid.NewGuid().ToString()
 
   [<Benchmark>]
   member _.NanoidNet() = Nanoid.Generate()
@@ -32,41 +32,110 @@ type AnanoidVsNanoidNetVsGuid() =
 [<SimpleJob(RunStrategy.Throughput)>]
 type AllAlphabets() =
   [<Benchmark>]
-  member me.Alphanumeric() = nanoIdOf Alphanumeric Defaults.Size
+  member _.Alphanumeric() = nanoIdOf Alphanumeric Defaults.Size
 
   [<Benchmark>]
-  member me.HexadecimalLowercase() = nanoIdOf HexadecimalLowercase Defaults.Size
+  member _.HexadecimalLowercase() = nanoIdOf HexadecimalLowercase Defaults.Size
 
   [<Benchmark>]
-  member me.HexadecimalUppercase() = nanoIdOf HexadecimalUppercase Defaults.Size
+  member _.HexadecimalUppercase() = nanoIdOf HexadecimalUppercase Defaults.Size
 
   [<Benchmark>]
-  member me.Lowercase() = nanoIdOf Lowercase Defaults.Size
+  member _.Lowercase() = nanoIdOf Lowercase Defaults.Size
 
   [<Benchmark>]
-  member me.NoLookalikes() = nanoIdOf NoLookalikes Defaults.Size
+  member _.NoLookalikes() = nanoIdOf NoLookalikes Defaults.Size
 
   [<Benchmark>]
-  member me.NoLookalikesSafe() = nanoIdOf NoLookalikesSafe Defaults.Size
+  member _.NoLookalikesSafe() = nanoIdOf NoLookalikesSafe Defaults.Size
 
   [<Benchmark>]
-  member me.Numbers() = nanoIdOf Numbers Defaults.Size
+  member _.Numbers() = nanoIdOf Numbers Defaults.Size
 
   [<Benchmark>]
-  member me.Uppercase() = nanoIdOf Uppercase Defaults.Size
+  member _.Uppercase() = nanoIdOf Uppercase Defaults.Size
 
   [<Benchmark(Baseline = true)>]
-  member me.UrlSafe() = nanoIdOf UrlSafe Defaults.Size
+  member _.UrlSafe() = nanoIdOf UrlSafe Defaults.Size
 
 
 [<MemoryDiagnoser>]
 [<SimpleJob(RunStrategy.Throughput)>]
 type FunctionVsStruct() =
   [<Benchmark(Baseline = true)>]
-  member me.Function() = nanoId ()
+  member _.Function() = nanoId ()
 
   [<Benchmark>]
-  member me.Struct() = NanoId.NewId()
+  member _.Struct() = NanoId.NewId()
+
+
+[<MemoryDiagnoser>]
+[<SimpleJob(RunStrategy.Throughput)>]
+type VaryingSizes() =
+  [<Params(8, 21, 36, 64, 128, 256)>]
+  member val Size = 0 with get, set
+
+  [<Benchmark>]
+  member me.Default() = nanoIdOf Defaults.Alphabet me.Size
+
+
+[<MemoryDiagnoser>]
+[<SimpleJob(RunStrategy.Throughput)>]
+type Pow2VsNonPow2() =
+  // 64 chars -> power-of-2 -> generatePow2
+  [<Benchmark(Baseline = true)>]
+  member _.Pow2_UrlSafe() = nanoIdOf UrlSafe Defaults.Size
+
+  // 62 chars -> non-power-of-2 -> generate (with rejection sampling)
+  [<Benchmark>]
+  member _.NonPow2_Alphanumeric() = nanoIdOf Alphanumeric Defaults.Size
+
+  // 16 chars -> power-of-2
+  [<Benchmark>]
+  member _.Pow2_Hex() = nanoIdOf HexadecimalLowercase Defaults.Size
+
+  // 10 chars -> non-power-of-2
+  [<Benchmark>]
+  member _.NonPow2_Numbers() = nanoIdOf Numbers Defaults.Size
+
+
+[<MemoryDiagnoser>]
+[<SimpleJob(RunStrategy.Throughput)>]
+type AlphabetStructVsRawFunction() =
+  let alphabet = Alphabet.makeOrRaise UrlSafe
+
+  [<Benchmark(Baseline = true)>]
+  member _.RawFunction() = nanoIdOf UrlSafe Defaults.Size
+
+  [<Benchmark>]
+  member _.AlphabetStruct() = Alphabet.makeNanoId Defaults.Size alphabet
+
+
+[<MemoryDiagnoser>]
+[<SimpleJob(RunStrategy.Throughput)>]
+type Parsing() =
+  let alphabet = Alphabet.makeOrRaise UrlSafe
+  let valid = string (alphabet.MakeNanoId Defaults.Size)
+  let invalid = String.replicate Defaults.Size "!"
+
+  [<Benchmark>]
+  member _.ParseValid() = Alphabet.parseNanoId valid alphabet
+
+  [<Benchmark>]
+  member _.ParseInvalid() = Alphabet.parseNanoId invalid alphabet
+
+  [<Benchmark>]
+  member _.ParseEmpty() = Alphabet.parseNanoId "" alphabet
+
+
+[<MemoryDiagnoser>]
+[<SimpleJob(RunStrategy.Throughput)>]
+type TaggedVsUntagged() =
+  [<Benchmark(Baseline = true)>]
+  member _.Untagged() = nanoId ()
+
+  [<Benchmark>]
+  member _.Tagged() = Tagged.nanoId' ()
 
 
 module Program =
@@ -74,6 +143,11 @@ module Program =
     typeof<AnanoidVsNanoidNetVsGuid>
     typeof<FunctionVsStruct>
     typeof<AllAlphabets>
+    typeof<VaryingSizes>
+    typeof<Pow2VsNonPow2>
+    typeof<AlphabetStructVsRawFunction>
+    typeof<Parsing>
+    typeof<TaggedVsUntagged>
   |]
 
   [<EntryPoint>]
